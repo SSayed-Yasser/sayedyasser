a99e923407e65aaf238bff530ec628fd1be2a096
SSayed-Yasser <sayedyassersy1@gmail.com>
2024-11-05 18:05:22 +0200

    Delete index directory

--- index/script/login.js
+++
@@ -1,8 +0,0 @@
-//check if login
-//localStorage.clear()
-if ( localStorage.getItem("login2") === 'passed11' ) {
-    console.log('pass');
-} else {
-    window.location.href = ("../index.html");
-}
-//check if login
