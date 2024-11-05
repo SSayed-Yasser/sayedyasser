23142d0e6c04c81769595e7cec6d84d16b822cd2
SSayed-Yasser <sayedyassersy1@gmail.com>
2024-11-05 18:05:56 +0200

    Add files via upload

---
+++ script/login.js
@@ -0,0 +1,8 @@
+//check if login
+//localStorage.clear()
+if ( localStorage.getItem("login2") === 'passed11' ) {
+    console.log('pass');
+} else {
+    window.location.href = ("../index.html");
+}
+//check if login
